5c5f2799f0cacef3249a74112bfb8be84bdd5f9b
narumincho <narumincho.starfy@gmail.com>
2021-03-19 14:45:37 +0900

    feat(wasm): 型の指定の意味がわかるように

--- wasm/main.ts
+++ wasm/main.ts
@@ -1,3 +1,7 @@
+import { functionTypeToWasmBinary } from "./type";
+import { uInt32ToBinary } from "./value";
+import { vectorToWasmBinary } from "./vector";
+
 export type Wasm = {
   functionList: ReadonlyArray<WasmFunction>;
 };
@@ -26,15 +30,6 @@ const wasmBinaryMagic: ReadonlyArray<number> = [0x00, 0x61, 0x73, 0x6d];
 const wasmBinaryVersion: ReadonlyArray<number> = [0x01, 0x00, 0x00, 0x00];
 
 /**
- * よく使われる要素数と中身
- */
-const vectorToWasmBinary = (
-  binaryList: ReadonlyArray<ReadonlyArray<number>>
-) => {
-  return [...uInt32ToBinary(binaryList.length), ...binaryList.flat()];
-};
-
-/**
  * セクションの内容をバイナリ形式に変換する
  */
 const sectionToWasmBinary = (
@@ -53,7 +48,10 @@ const typeSection = (): ReadonlyArray<number> => {
   /**
    * とりあえず ():i32 だけ
    */
-  const i32Type: ReadonlyArray<number> = [0x60, 0x00, 0x01, 0x7f];
+  const i32Type: ReadonlyArray<number> = functionTypeToWasmBinary({
+    parameter: [],
+    result: ["i32"],
+  });
 
   return sectionToWasmBinary(0x01, vectorToWasmBinary([i32Type]));
 };
@@ -123,106 +121,6 @@ const codeSection = (
   const body = functionList.map((func) => optToBinary(func.expr));
 
   return sectionToWasmBinary(0x0a, vectorToWasmBinary(body));
-};
-/**
- * 1番左のビットをONにする
- * ```
- * 010101011
- * ↓
- * 110101011
- * ```
- */
-const onLeftBit = (value: number): number => {
-  return value | 0b10000000;
-};
-
-/**
- * 右端から数えて場所から7bit分を取得する
- * ```
- * 101010101011
- * ↓ offset 2
- *    0101010
- * ```
- */
-const get7Bits = (value: number, offset: number): number => {
-  return (value >> offset) & 0b1111111;
-};
-
-/**
- * 8bit を表示する. デバッグ用
- */
-const byteToString = (value: number): string => {
-  return value.toString(2).padStart(8, "0");
-};
-
-/**
- * https://github.com/sunfishcode/wasm-reference-manual/blob/master/WebAssembly.md#primitive-encoding-types"
- *
- * 符号付き32bit整数1byte ～ 5 byteの可変長のバイナリに変換する
- * LEB128
- */
-const int32ToBinary = (x: number): ReadonlyArray<number> => {
-  /** 0~6 ビット目 */
-  const b0 = get7Bits(x, 0);
-  /** 7~13 ビット目 */
-  const b1 = get7Bits(x, 7);
-  /** 14~20 ビット目 */
-  const b2 = get7Bits(x, 14);
-  /** 21~27 ビット目 */
-  const b3 = get7Bits(x, 21);
-  /** 28~31 ビット目. (32~34bitは常にON) */
-  const b4 = get7Bits(x, 28) | 0b01110000;
-
-  if (-(2 ** 6) <= x && x <= 2 ** 6 - 1) {
-    return [b0];
-  }
-  if (-(2 ** 13) <= x && x <= 2 ** 13 - 1) {
-    return [onLeftBit(b0), b1];
-  }
-  if (-(2 ** 20) <= x && x <= 2 ** 20 - 1) {
-    return [onLeftBit(b0), onLeftBit(b1), b2];
-  }
-
-  if (-(2 ** 27) <= x && x <= 2 ** 27 - 1) {
-    return [onLeftBit(b0), onLeftBit(b1), onLeftBit(b2), b3];
-  }
-
-  return [onLeftBit(b0), onLeftBit(b1), onLeftBit(b2), onLeftBit(b3), b4];
-};
-
-/**
- * https://github.com/sunfishcode/wasm-reference-manual/blob/master/WebAssembly.md#primitive-encoding-types
- *
- * 符号なし32bit整数を1byte ～ 5 byteの可変長のバイナリに変換する
- * LEB128
- */
-const uInt32ToBinary = (x: number): ReadonlyArray<number> => {
-  /** 0~6 ビット目 */
-  const b0 = get7Bits(x, 0);
-  /** 7~13 ビット目 */
-  const b1 = get7Bits(x, 7);
-  /** 14~20 ビット目 */
-  const b2 = get7Bits(x, 14);
-  /** 21~27 ビット目 */
-  const b3 = get7Bits(x, 21);
-  /** 28~31 ビット目. (32~34bitは常にOFF) */
-  const b4 = get7Bits(x, 28);
-
-  if (b1 === 0 && b2 === 0 && b3 === 0 && b4 === 0) {
-    return [b0];
-  }
-  if (b2 === 0 && b3 === 0 && b4 === 0) {
-    return [onLeftBit(b0), b1];
-  }
-  if (b3 === 0 && b4 === 0) {
-    return [onLeftBit(b0), onLeftBit(b1), b2];
-  }
-
-  if (b4 === 0) {
-    return [onLeftBit(b0), onLeftBit(b1), onLeftBit(b2), b3];
-  }
-
-  return [onLeftBit(b0), onLeftBit(b1), onLeftBit(b2), onLeftBit(b3), b4];
 };
 
 type WatLike =
---
+++ wasm/type.ts
@@ -0,0 +1,33 @@
+import { vectorToWasmBinary } from "./vector";
+
+export type NumType = "i32" | "i64" | "f32" | "f64";
+
+export const numTypeToWasmBinary = (
+  numType: NumType
+): ReadonlyArray<number> => {
+  switch (numType) {
+    case "i32":
+      return [0x7f];
+    case "i64":
+      return [0x7e];
+    case "f32":
+      return [0x7d];
+    case "f64":
+      return [0x7c];
+  }
+};
+
+export type FunctionType = {
+  parameter: ReadonlyArray<NumType>;
+  result: ReadonlyArray<NumType>;
+};
+
+export const functionTypeToWasmBinary = (
+  functionType: FunctionType
+): ReadonlyArray<number> => {
+  return [
+    0x60,
+    ...vectorToWasmBinary(functionType.parameter.map(numTypeToWasmBinary)),
+    ...vectorToWasmBinary(functionType.result.map(numTypeToWasmBinary)),
+  ];
+};
